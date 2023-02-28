a3d950764cee28bb7b77b77ae8c02d68ae0101b2
TapanMandal <tapan.mca7@gmail.com>
2023-02-28 23:55:27 +0530

    Updated the Application Exception

--- src/main/java/com/custom/validation/advice/ApplicationExceptionHandler.java
+++ src/main/java/com/custom/validation/advice/ApplicationExceptionHandler.java
@@ -2,6 +2,7 @@ package com.custom.validation.advice;
 
 import com.custom.validation.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,21 +14,19 @@ import java.util.Map;
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException exception) {
+    public ResponseEntity<Map<String, String>> handleInvalidArgument(MethodArgumentNotValidException exception) {
         Map<String, String> errorMap = new HashMap<>();
         exception.getBindingResult().getFieldErrors().forEach(error -> {
             errorMap.put(error.getField(), error.getDefaultMessage());
         });
-        return errorMap;
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(UserNotFoundException.class)
-    public Map<String, String> handleInvalidArgument(UserNotFoundException ex) {
+    public ResponseEntity<Map<String, String>> handleInvalidArgument(UserNotFoundException ex) {
         Map<String, String> errorMap = new HashMap<>();
         errorMap.put("No User with Id", ex.getMessage());
-        return errorMap;
+        return new ResponseEntity<>(errorMap, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
