b243f0d097cc30172c986c9dd5493fe4c31c40c3
TapanMandal <tapan.mca7@gmail.com>
2023-04-05 08:01:59 +0530

    UserService Unit Test

--- src/test/java/com/custom/validation/service/UserServiceTest.java
+++ src/test/java/com/custom/validation/service/UserServiceTest.java
@@ -2,18 +2,32 @@ package com.custom.validation.service;
 
 import com.custom.validation.dto.UserRequest;
 import com.custom.validation.entity.User;
+import com.custom.validation.exception.UserNotFoundByEmailException;
 import com.custom.validation.exception.UserNotFoundException;
 import com.custom.validation.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.data.domain.Sort.Direction.ASC;
 
 @ExtendWith(SpringExtension.class)
 class UserServiceTest {
@@ -55,7 +69,7 @@ class UserServiceTest {
     @Test
     void searchUserInvalid() {
         int userId = 0;
-        Throwable exception = assertThrows(UserNotFoundException.class, ()->{
+        Throwable exception = assertThrows(UserNotFoundException.class, () -> {
             service.searchUser(userId);
         });
         assertEquals("User Not Found with id :" + userId, exception.getMessage());
@@ -64,42 +78,128 @@ class UserServiceTest {
     @Test
     void getUserByUserIdAndName() {
         int userId = 1;
-        String userName ="Tapan";
+        String userName = "Tapan";
         List<User> users = UserServiceFactory.getUserList();
         given(repository.findAll()).willReturn(users);
         User result = service.getUserByUserIdAndName(userId, userName);
         assertNotNull(result);
     }
 
+    @Disabled // TODO - Not Working as expected
+    @TestFactory
+    Stream<DynamicTest> getUserByUserIdAndNameDynamic() {
+        int userId = 1;
+        // String userName ="Tapan";
+        List<User> users = UserServiceFactory.getUserList();
+        given(repository.findAll()).willReturn(users);
+        return users.stream().map(user -> DynamicTest.dynamicTest("User Id ", () -> assertEquals(userId, user.getUserId())));
+    }
+
+    @Disabled // TODO - Not working as expected
+    @Test
+    public void getUserByUserIdAndNameLoop() {
+        int userId = 1;
+        String userName = "Tapan";
+        List<User> users = UserServiceFactory.getUserList();
+        User user = repository.findByUserId(userId);
+        assertAll("Mandal User Id and Name",
+                () -> assertEquals(1, user.getUserId()),
+                () -> assertEquals("Tapan", user.getName())
+        );
+    }
+
     @Test
     void getUserByUserIdAndNameStream() {
+        int userId = 1;
+        String userName = "Tapan";
+        List<User> users = UserServiceFactory.getUserList();
+        given(repository.findAll()).willReturn(users);
+        Optional<User> result = service.getUserByUserIdAndNameStream(userId, userName);
+        assertNotNull(result);
     }
 
     @Test
     void sortFields() {
+        String field = "name";
+        List<User> users = UserServiceFactory.getUserList();
+        given(repository.findAll(Sort.by(ASC, field)).stream().collect(Collectors.toList())).willReturn(users);
+        List<User> result = service.sortFields(field);
+        assertNotNull(result);
     }
 
     @Test
     void userPagination() {
+        int offset = 1;
+        int pageSize = 2;
+        List<User> users = UserServiceFactory.getUserList();
+        Page<User> page = new PageImpl<>(users);
+        given(repository.findAll(PageRequest.of(offset, pageSize))).willReturn(page);
+        Page<User> result = service.userPagination(offset, pageSize);
+        assertNotNull(result);
     }
 
     @Test
     void userPaginationAndSorting() {
+        int offset = 1;
+        int pageSize = 2;
+        String fieldName = "name";
+        List<User> users = UserServiceFactory.getUserList();
+        Page<User> page = new PageImpl<>(users);
+        given(repository.findAll(PageRequest.of(offset, pageSize).withSort(Sort.by(fieldName)))).willReturn(page);
+        Page<User> result = service.userPaginationAndSorting(offset, pageSize, fieldName);
+        assertNotNull(result);
     }
 
+    @Disabled // Giving Null for all the Disabled
     @Test
     void updateUserDetails() {
+        int userId = 1;
+        User user = repository.findByUserId(userId);
+        given(repository.findByUserId(userId)).willReturn(user);
+        given(repository.save(user)).willReturn(user);
+        User result = service.updateUserDetails(userId, user);
+        assertNotNull(result);
     }
 
+    @Disabled
     @Test
     void partiallyUpdateUser() {
+        int uid = 1;
+        User user = repository.findByUserId(uid);
+        Map<String, Object> fields = new HashMap<>();
+        given(repository.findByUserId(uid)).willReturn(user);
+        given(repository.save(user)).willReturn(user);
+        User userUpdated = service.partiallyUpdateUser(uid, fields);
+        assertNotNull(userUpdated);
     }
 
+    @Disabled
     @Test
     void updateUserDetailsWithNoUserId() {
+        int uid = 1;
+        User user = repository.findByUserId(uid);
+        List<User> users = UserServiceFactory.getUserList();
+        given(repository.findAll()).willReturn(users);
+        given(repository.save(user)).willReturn(user);
+        User result = service.updateUserDetailsWithNoUserId(user);
+        assertNotNull(result);
+    }
+    @Disabled
+    @Test
+    void getUserByUserEmail() throws UserNotFoundByEmailException {
+        String email = "tapan.tapan@gmail.com";
+        User user = repository.findByEmail(email);
+        given(repository.findByEmail(email)).willReturn(user);
+        User result = service.getUserByUserEmail(email);
+        assertNotNull(result);
     }
 
     @Test
-    void getUserByUserEmail() {
+    void getUserByUserEmailInvalid() {
+        String email = null;
+        Throwable exception = assertThrows(UserNotFoundByEmailException.class, () -> {
+            service.getUserByUserEmail(email);
+        });
+        assertEquals("user not found by email :" + email, exception.getMessage());
     }
 }
