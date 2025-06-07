1fac9963262faa724fb9e5d2ae1199805504c6cf
TapanMandal <tapan.mca7@gmail.com>
2025-06-07 06:12:08 +0530

    User Defined Exception with all constructor

--- src/main/java/com/custom/validation/exception/UserNotFoundByEmailException.java
+++ src/main/java/com/custom/validation/exception/UserNotFoundByEmailException.java
@@ -1,7 +1,23 @@
 package com.custom.validation.exception;
 
-public class UserNotFoundByEmailException extends Exception{
+public class UserNotFoundByEmailException extends Exception {
+    //Just create constructor of this class, it will generate the following stuffs
+    public UserNotFoundByEmailException() {
+    }
+
     public UserNotFoundByEmailException(String message) {
         super(message);
+    }
+
+    public UserNotFoundByEmailException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundByEmailException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserNotFoundByEmailException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- src/main/java/com/custom/validation/exception/UserNotFoundException.java
+++ src/main/java/com/custom/validation/exception/UserNotFoundException.java
@@ -1,7 +1,24 @@
 package com.custom.validation.exception;
 
-public class UserNotFoundException extends Exception{
+public class UserNotFoundException extends Exception {
+    //Just create constructor of this class, it will generate the below stuffs
+    public UserNotFoundException() {
+        super();
+    }
+
     public UserNotFoundException(String message) {
         super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
